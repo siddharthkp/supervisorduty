a3c4f4f2dc3610cb54a7a0fa04c2f268f8dc2704
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-01-22 06:56:27 +0000

    Prettified instructions

--- setup.js
+++ setup.js
@@ -1,6 +1,10 @@
 var inquirer = require('inquirer');
 var fs = require('fs');
-console.log('Let\'s set up supervisorduty');
+var chalk = require('chalk');
+
+console.log(chalk.blue('\n\n\n\------------------------------ \n'));
+console.log(chalk.blue('Let\'s set up supervisorduty \n'));
+
 var questions = [
     {
         name: 'server_name',
@@ -16,9 +20,10 @@ inquirer.prompt(questions, function(answers) {
     fs.writeFile(__dirname + '/config.json', JSON.stringify(answers), function(err) {
         if(err) return console.log(err);
         else {
-            console.log('Config file created');
-	    console.log('Add the following to your cron file:');
-	    console.log('*/15 * * * * node ' + __dirname + ' & npm start');
+            console.log(chalk.blue('Config file created'));
+	    console.log(chalk.blue('Add the following to your cron file:'));
+	    console.log(chalk.blue('*/15 * * * * node ' + __dirname + ' & npm start'));
+	    console.log(chalk.blue('\n------------------------------\n\n\n'));
 	}
     });
 });
